ffec5c02118f82ee19e5ae3dcc2b0f8fb3275aba
Huevos <rowland@moralet.com>
2025-01-15 22:46:45 +0100

    [CurrentService.py] add comment

--- lib/python/Components/Sources/CurrentService.py
+++ lib/python/Components/Sources/CurrentService.py
@@ -57,6 +57,9 @@ class CurrentService(PerServiceBase, Source):
 			return NavigationInstance.instance.getCurrentlyPlayingServiceOrGroup()
 		return None
 
+	# Setter added for iptv-m3u-reader plugin. For more info see:
+	# https://github.com/OpenViX/enigma2/commit/8002af0e452e99f3d49aea326ab27bc4b1b4b6e4
+	# https://github.com/DimitarCC/iptv-m3u-reader/commit/08d93986f0f00de6d0ff172abd0bb7b641921bda#diff-834f0679bc61d0427df924d74c20767d5b8fa62227d824361dd81555df6f6725R477-R482
 	def setCurrentServiceRef(self, ref):
 		self.ref = ref
 
