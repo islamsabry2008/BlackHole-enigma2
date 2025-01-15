8002af0e452e99f3d49aea326ab27bc4b1b4b6e4
Dimitar Tsenev <dimitar_tsenev@outlook.com>
2025-01-15 09:22:11 +0200

    [Updated] Add setter to serviceref

--- lib/python/Components/Sources/CurrentService.py
+++ lib/python/Components/Sources/CurrentService.py
@@ -25,6 +25,7 @@ class CurrentService(PerServiceBase, Source):
 		self.navcore = navcore
 		self.srv = None
 		self.info = None
+		self.ref = None
 		self.onManualNewService = []
 
 	def serviceEvent(self, event):
@@ -50,11 +51,16 @@ class CurrentService(PerServiceBase, Source):
 
 	@cached
 	def getCurrentServiceRef(self):
+		if self.ref:
+			return self.ref
 		if NavigationInstance.instance is not None:
 			return NavigationInstance.instance.getCurrentlyPlayingServiceOrGroup()
 		return None
+	
+	def setCurrentServiceRef(self, ref):
+		self.ref = ref
 
-	serviceref = property(getCurrentServiceRef)
+	serviceref = property(getCurrentServiceRef, setCurrentServiceRef)
 
 	def newService(self, ref):
 		if ref and isinstance(ref, bool):
