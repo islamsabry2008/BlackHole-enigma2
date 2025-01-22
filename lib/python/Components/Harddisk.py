92fe32eb4a36d64741c2503e3fac9f99213d8008
TwolDE <tonywhalley@t-online.de>
2025-01-22 09:19:33 +0100

    [Harddisk] mount DM recovery partition at 1st boot

--- lib/python/Components/Harddisk.py
+++ lib/python/Components/Harddisk.py
@@ -680,7 +680,7 @@ class HarddiskManager:
 			mounts = getProcMounts()
 			devmounts = [x[0] for x in mounts]
 			mounts = [x[1] for x in mounts if x[1].startswith("/media/")]
-			possibleMountPoints = [f"/media/{x}" for x in ("usb8", "usb7", "usb6", "usb5", "usb4", "usb3", "usb2", "usb", "hdd") if f"/media/{x}" not in mounts]
+			possibleMountPoints = [f"/media/{x}" for x in ("usb8", "usb7", "usb6", "usb5", "usb4", "usb3", "usb2", "usb", "data", "hdd") if f"/media/{x}" not in mounts]
 
 			for device in devices:
 				if device["DEVNAME"] not in devmounts or "/media/hdd" in possibleMountPoints:
@@ -814,9 +814,18 @@ class HarddiskManager:
 							if devMajor == 179 and boxModel in ("dm900", "dm920") and partition != "mmcblk0p3":
 								continue
 							description = self.getUserfriendlyDeviceName(partition, physicalDevice)
-							print(f"[Harddisk][enumerateBlockDevices] Found partition '{partition}', description='{description}', device='{physicalDevice}'.")
-							# part = Partition(mountpoint=self.getMountpoint(partition), description=description, force_mounted=True, device=partition)
-							part = Partition(mountpoint=self.getMountpoint(partition, skiproot=True), description=description, force_mounted=True, device=partition)
+							print(f"[Harddisk][enumerateBlockDevices]### Found partition '{partition}', description='{description}', device='{physicalDevice}' mountpoint='{self.getMountpoint(partition)}.")
+							if boxModel in ("dm900", "dm920") and partition == "mmcblk0p3" and self.getMountpoint(partition) == None:
+								mountpoint = "/media/data/"
+								newFstab = fileReadLines("/etc/fstab")
+								newFstab.append("/dev/mmcblk0p3 /media/data ext4 rw, relatime,data=ordered 0 0")
+								fileWriteLines("/etc/fstab", newFstab)
+								if not ospath.exists(mountpoint):
+									mkdir(mountpoint, 0o755)
+								self.console.ePopen("/bin/mount -a")
+								part = Partition(mountpoint, description=description, force_mounted=True, device=partition)
+							else:
+								part = Partition(mountpoint=self.getMountpoint(partition, skiproot=True), description=description, force_mounted=True, device=partition)
 							self.partitions.append(part)
 							# print(f"[Harddisk][enumerateBlockDevices]  Partition(mountpoint = {self.getMountpoint(partition)}, description = {description}, force_mounted = True, device = {partition})")
 							self.on_partition_list_change("add", part)
