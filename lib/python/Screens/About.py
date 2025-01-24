b7a9a40d3eeeafe3555256108cc8868d04294517
Ev0-BH <Ev0_BhT@outlook.com>
2025-01-24 14:03:52 +0000

    [About] Tweak display to include SD cards

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -284,9 +284,11 @@ class Devices(Screen):
 				hddp = self.hddlist[count][0]
 				if "ATA" in hddp:
 					hddp = hddp.replace("ATA ", "", 2)
-					hddp = hddp.replace("SATA ", "SATA Internal Bus ").replace("(HDD)", "HDD").replace(" (", "").replace(")", "")
+					hddp = hddp.replace("SATA ", "SATA Internal Bus ").replace("(", "").replace(")", "").replace("   ", " ").replace("  ", " ").replace("/dev", " /dev")
 				if "USB" in hddp:
-					hddp = hddp.replace(" (SD", "(SD").replace("(", "").replace(") ", "")
+					hddp = hddp.replace("(", "").replace(")", "").replace("   ", " ").replace("  ", " ").replace("/dev", " /dev")
+				if "SD" in hddp:
+					hddp = hddp.replace("(", "").replace(")", "").replace("   ", " ").replace("  ", " ").replace("/dev", " /dev")
 				free = hdd.Totalfree()
 				if free >= 1:
 					free *= 1000000 # convert MB to bytes
